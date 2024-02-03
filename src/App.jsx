d3978cfff78bcaf087e46093f3aa13fa7061c5be
Dhabaldeep <deepdhabal2001@gmail.com>
2024-02-03 16:07:01 +0530

    Back to previous version

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,10 @@
 import MapComponent from "./components/MapComponent";
 
 const App = () => {
-  const vehicles = [
-    { vehicleId: "123456", name: "Scorpio" },
-    // { vehicleId: "1234567", name: "Omni" },
-    // Add more vehicles as needed
-  ];
   return (
     <div>
       <h1>Your App</h1>
-      {vehicles.map((vehicle) => (
-        <MapComponent key={vehicle.vehicleId} vehicleId={vehicle.vehicleId} />
-      ))}
-
+      <MapComponent />
       {/* Other components */}
     </div>
   );
