1f6a5411833d69e92eedcf52cf3281b52c3210bb
Dhabaldeep <deepdhabal2001@gmail.com>
2024-03-06 12:13:06 +0530

    fix the dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-const theme = createTheme({ /* Your theme options */ });
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// const theme = createTheme({ /* Your theme options */ });
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-
-  </React.StrictMode>,
-)
+    {/* <ThemeProvider theme={theme}> */}
+    <App />
+    {/* </ThemeProvider> */}
+  </React.StrictMode>
+);
